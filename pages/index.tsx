41e7b092317e5ff63285e1e45cb4add3bcb566d7
wjdghks963 <chsw000@naver.com>
2022-04-30 23:39:25 +0900

    tailwind CSS TEST2

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,28 @@ return (
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className='bg-blue-500 p-6 pb-14' >
+          <span className='text-white text-2xl'>Profile</span>
+        </div>
+        <div className='rounded-3xl p-6 bg-white relative -top-5'>
+          <div className='flex relative -top-16 items-end justify-between'>
+            <div className='flex flex-col items-center'>
+            <span className='text-sm text-gray-500'>Oders</span>
+            <span className='font-medium'>340</span>
+            </div>
+            <div  className='h-24 w-24 bg-red-400 rounded-full'/>
+            <div className='flex flex-col items-center'>
+              <span className='text-sm text-gray-500'>Spent</span>
+              <span className='font-medium'>$2310</span>
+            </div>
+          </div>
+          <div className='relative mt-5 flex flex-col items-center -mt-10 mb-5'>
+              <span className='text-lg font-medium'>Tony Molly</span>
+              <span className='text-sm text-gray-500'>미국</span>
+            </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
