47da6eb0d473a344cb96b699111aa924d69e538c
wjdghks963 <chsw000@naver.com>
2022-05-22 23:35:28 +0900

    컴포넌트 분리 중 & Layout.tsx 완성

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,21 +1,24 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="divide-y-[1px] py-10">
-      {[1, 11, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="mb-3 flex cursor-pointer items-center space-x-3 px-4 py-3 pb-3"
-        >
-          <div className="h-10 w-10 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jebs</p>
-            <p className="text-sm  text-gray-500">내일 봅시다</p>
+    <Layout hasTabBar title="채팅">
+      <div className="divide-y-[1px] py-10">
+        {[1, 11, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="mb-3 flex cursor-pointer items-center space-x-3 px-4 py-3 pb-3"
+          >
+            <div className="h-10 w-10 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jebs</p>
+              <p className="text-sm  text-gray-500">내일 봅시다</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
---
+++ pages/live/index.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from "next";
+import Layout from "../../components/layout";
+
+const Live: NextPage = () => {
+  return (
+    <Layout hasTabBar title="라이브 방송">
+      <div className="space-y-5 divide-y-2 py-10 ">
+        {[1, 1, 1, 1].map((_, i) => (
+          <div key={i} className="px-4 pt-4">
+            <div className="aspect-video w-full rounded-md bg-slate-400 shadow-sm" />
+            <h3 className="mt-2 text-lg text-gray-700">영상 제목</h3>
+          </div>
+        ))}
+        <button className="fixed bottom-24 right-5 rounded-full border-transparent bg-orange-400 p-4 text-white shadow-xl transition-colors hover:bg-orange-500">
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Live;
