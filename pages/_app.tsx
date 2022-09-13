4875a6bea5800347cea4b1bf99e698f723fafc29
wjdghks963 <chsw000@naver.com>
2022-09-13 23:07:40 +0900

    userUser hook 변경 & edit page useForm 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
+import useUser from "@libs/client/useUser";
+
+function IsLoggedIn() {
+  const { user } = useUser();
+  return null;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       }}
     >
       <div className="mx-auto w-full max-w-xl">
+        <IsLoggedIn />
         <Component {...pageProps} />
       </div>
     </SWRConfig>
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,12 +1,40 @@
+import useUser from "@libs/client/useUser";
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../../components/button";
 import Input from "../../components/input";
 import Layout from "../../components/layout";
 
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
+
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const {
+    register,
+    setValue,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [setValue, user]);
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === "" && phone === "") {
+      setError("formErrors", {
+        message: "이메일이나 핸드폰 중 하나가 꼭 필요합니다.",
+      });
+    }
+  };
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="space-y-4 py-10 px-4">
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 py-10 px-4">
         <div className="flex items-center space-x-3">
           <div className="h-14 w-14 rounded-full bg-slate-500" />
           <label
@@ -22,14 +50,26 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
         <Input
-          required
+          register={register("email")}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
+          required={false}
           label="Phone number"
           name="phone"
           type="number"
           kind="phone"
         />
+        {errors.formErrors ? (
+          <span className="my-2 block text-center font-medium text-red-500">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
         <Button text="Update profile" />
       </form>
     </Layout>
